f80ade738ee8007a405e913f62212bc05cbbfeb6
ThienTho123 <thientho7a4@gmail.com>
2024-01-27 20:27:19 +0700

    add jump to character

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,15 +11,19 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float runspeed = 5f;
     Vector2 moveInput;
     Rigidbody2D myrigidbody;
+
+    Animator myAnimator;
     void Start()
     {
         myrigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     void Update()
     {
         Run();
         FlipSprite();
+
     }
     
     void OnMove(InputValue value)
@@ -32,6 +36,8 @@ public class PlayerMovement : MonoBehaviour
         Vector2 PlayerVelocity = new Vector2(moveInput.x*runspeed, myrigidbody.velocity.y);
         myrigidbody.velocity=PlayerVelocity;
         
+        bool playerHorizontalSpeed = Mathf.Abs(myrigidbody.velocity.x)>Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerHorizontalSpeed);
 
     }
     void FlipSprite()
@@ -40,6 +46,10 @@ public class PlayerMovement : MonoBehaviour
             if(playerHorizontalSpeed)
             {
             transform.localScale = new Vector2 (Mathf.Sign(myrigidbody.velocity.x),1f);
+            }
+            if(myAnimator)
+            {
+
             }        
 
         }
