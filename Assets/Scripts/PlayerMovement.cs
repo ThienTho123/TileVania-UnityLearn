7039ec0e649cb199d381fb28c2a2d09b67a820e4
ThienTho123 <thientho7a4@gmail.com>
2024-01-30 21:00:19 +0700

    add gun

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float jumpSpeed =5f;
     [SerializeField] float climbSpeed = 5f;
     [SerializeField] Vector2 deathKick = new Vector2 (10f, 10f);
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform gun;
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
     Animator myAnimator;
@@ -54,6 +56,11 @@ public class PlayerMovement : MonoBehaviour
         {
             myRigidbody.velocity += new Vector2(0f,jumpSpeed);
         }
+    }
+    void OnFire(InputValue value)
+    {   
+        if(!isAlive) {return;}
+        Instantiate(bullet, gun.position, transform.rotation);
     }
     void Run()
     {
