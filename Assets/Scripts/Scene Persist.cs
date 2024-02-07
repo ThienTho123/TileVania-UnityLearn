0fd119cef27932068a527365afb6953ad4904baa
ThienTho123 <thientho7a4@gmail.com>
2024-02-07 22:17:42 +0700

    update map

---
+++ Assets/Scripts/Scene Persist.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+
+public class ScenePersist : MonoBehaviour
+{
+    void Awake()
+    {
+        int numScenePersist = FindObjectsOfType<ScenePersist>().Length;
+        if (numScenePersist > 1)
+        {
+            Destroy(gameObject);
+        }
+        else 
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    public void ResetScenePersists()
+    {
+        
+    }
+}
